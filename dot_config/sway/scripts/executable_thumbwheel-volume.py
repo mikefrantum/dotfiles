6cc680ecbdcc907da29f14b95f780de92f57f6b5
Mike Frantum <mike@mikefrantum.dev>
2026-06-10 23:31:03 -0600

    add: thumbwheel volume script

---
+++ dot_config/sway/scripts/executable_thumbwheel-volume.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import evdev
+import subprocess
+
+DEVICE = "/dev/input/event6"
+REL_HWHEEL = 6
+
+device = evdev.InputDevice(DEVICE)
+
+for event in device.read_loop():
+    if event.type == evdev.ecodes.EV_REL and event.code == REL_HWHEEL:
+        if event.value > 0:
+            subprocess.run(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%"])
+        elif event.value < 0:
+            subprocess.run(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%"])
