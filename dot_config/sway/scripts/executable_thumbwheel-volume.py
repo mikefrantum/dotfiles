6eefc5e00c1d389bcfbe08246c13d9969516b161
Mike Frantum <mike@mikefrantum.dev>
2026-06-11 16:54:04 -0600

    fix volume on mouse thumbwheel

--- dot_config/sway/scripts/executable_thumbwheel-volume.py
+++ dot_config/sway/scripts/executable_thumbwheel-volume.py
@@ -10,6 +10,6 @@ device = evdev.InputDevice(DEVICE)
 for event in device.read_loop():
     if event.type == evdev.ecodes.EV_REL and event.code == REL_HWHEEL:
         if event.value > 0:
-            subprocess.run(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%"])
-        elif event.value < 0:
             subprocess.run(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%"])
+        elif event.value < 0:
+            subprocess.run(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%"])
