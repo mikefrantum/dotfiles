c8d08d7845da7301a5954b6dd0ae0e9efce69fb1
Mike Frantum <mike@mikefrantum.dev>
2026-08-20 08:26:47 -0600

    updates to bashrc for nvim and options for nvim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.g.lazyvim_check_order = false
+
+vim.opt.ignorecase = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.number = true
+vim.opt.relativenumber = false
